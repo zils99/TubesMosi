1c721cb6626e2fdb6a5b31917b687f695bdd43dc
Daffa Pratama <61155437+dafapratama@users.noreply.github.com>
2020-04-29 22:53:50 +0700

    Penambahan Plot Animasi
    Membuat Plot Animasi dari Jumlah terinfeksi , rekovery, dan infected

---
+++ tubes.py
@@ -0,0 +1,146 @@
+#import library
+import random 
+import numpy as np
+import pandas as pd
+import matplotlib
+import time
+from os.path import exists
+import matplotlib.pyplot as plt
+
+#--------------Variabel Program-------------
+xMin = -20
+xMax = 20
+yMin = -20 
+yMax = 20
+x_range = xMax - xMin
+y_range = yMax - yMin
+nInfeksi = 0
+nIndividu = 200
+pulih = 0
+day = 0
+t = time.time()
+rasioIndividuTerinfeksi = 5/100
+probIndividuBergerak = 8/100
+waktuPulih = 10
+conTerinfeksi = True
+#-------------Prepare data-------------
+
+posX=[] #sumbu x 
+posY=[] #sumbu y
+stat_sehat=[] 
+stat_imun=[]
+waktu_infeksi=[]
+total_nIfeksi=[]
+
+dataAwal=pd.DataFrame()
+for i in range (nIndividu):
+    rndx = random.randint(-20, 20)
+    rndy = random.randint(-20, 20)
+    posX.append(rndx)
+    posY.append(rndy)
+    if i < 10 :
+        stat_sehat.append('terinfeksi')
+    else :
+        stat_sehat.append('sehat')
+    stat_imun.append('belum memiliki imun')
+    waktu_infeksi.append(0)
+#--------------Main Program-------------
+
+while conTerinfeksi==True:
+    for k in range (nIndividu):
+        x=posX[k]
+        y=posY[k]
+        #melakukan random bilangan float 0 dan 1
+        rand = random.uniform(0,1)
+        #right
+        if rand <= 0.20 :
+            x = x + 1
+            y = y
+        #down
+        elif rand <= 0.40 :
+            x = x 
+            y = y - 1
+        #left
+        elif rand <= 0.60 :
+            x = x - 1
+            y = y
+        #up
+        elif rand <= 0.80 :
+            x = x
+            y = y + 1
+        #diam
+        else :
+            x = x
+            y = y
+
+        #periodic boundary condition
+        #correction of x axis
+        if (x > xMax) :
+            x = x - 1
+            rnd = random.uniform(0,1)
+            if (rnd < rasioIndividuTerinfeksi):
+                stat_sehat[k]='terinfeksi'
+            else :
+                stat_sehat[k]='sehat'
+        if (x < xMin) :
+            x = x + 1
+            rnd = random.uniform(0,1)
+            if (rnd < rasioIndividuTerinfeksi):
+                stat_sehat[k]='terinfeksi'
+            else :
+                stat_sehat[k]='sehat'
+        #correction of y axis
+        if (y > yMax) :
+            y = y - 1
+            rnd = random.uniform(0,1)
+            if (rnd < rasioIndividuTerinfeksi):
+                stat_sehat[k]='terinfeksi'
+            else :
+                stat_sehat[k]='sehat'
+        if (y < yMin)  :
+            y = y + 1
+            rnd = random.uniform(0,1)
+            if (rnd < rasioIndividuTerinfeksi):
+                stat_sehat[k]='terinfeksi'
+            else :
+                stat_sehat[k]='sehat'
+
+                
+        #correction of jarak
+        for l in range (nIndividu):
+            if (posX[k]==posX[l] and posY[k]==posY[l]):
+                #penularan virus
+                if(stat_sehat[k]=='terinfeksi'):
+                    if(stat_sehat[l]=='sehat' and stat_imun[k]=='belum memiliki imun'):
+                        stat_sehat[k]='terinfeksi'
+                elif(stat_sehat[l]=='terinfeksi'):
+                    stat_sehat[k]='terinfeksi'
+                    
+        #correction of waktu_infeksi
+        if (stat_sehat[k]=='terinfeksi'):
+            if (waktu_infeksi[k] == waktuPulih) : 
+                stat_sehat[k]='Pulih'
+                stat_imun[k]='memiliki imun'
+                nInfeksi-=1
+                pulih+=1
+            elif (waktu_infeksi[k]==0) : 
+                waktu_infeksi[k]+=1
+                nInfeksi+=1
+            else :
+                waktu_infeksi[k]+=1
+                            
+        posX[k]=x
+        posY[k]=y
+    if (nInfeksi==0):
+        conTerinfeksi = False     
+    day += 1
+    total_nIfeksi.append(nInfeksi)
+    if day == 1:
+        plt.title('(Day-'+str(day)+') Infected = '+str(nInfeksi))
+    elif day>1:
+        plt.title('(Day-'+str(day)+') Infected = '+str(nInfeksi)+' Recovered = '+str(pulih))
+    plt.scatter(posX, posY)
+    plt.show()
+    
+print('VIRUS OVER on'+' Day-'+str(day))
+print('RECOVERED  '+str(pulih))
